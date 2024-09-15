f217a705bc1adcb4784cef5cd8bc81cb886309d9
godonggedang <godonggedang@airgodonggedang>
2024-09-15 20:40:13 +0700

    test

--- latihan/src/main/java/latihan1/latihan/latihanSegitigaSatu.java
+++ latihan/src/main/java/latihan1/latihan/latihanSegitigaSatu.java
@@ -11,21 +11,21 @@ import java.util.Scanner;
  * @author godonggedang
  */
 public class latihanSegitigaSatu {
-    
-    public static void main(String[] args){
+     
+    public static void main(String[] args){ 
         
         Scanner input = new Scanner(System.in);
         int i,j,result,inputTinggi;
         
         System.out.print("masukkan panjang segitiga : ");
         inputTinggi = input.nextInt();
-        result = 0; 
+        result = 0;
         
         for(i = 1; i<=inputTinggi; i++){
             result = result+i; 
             for(j = 1; j<= i ; j++){
                 System.out.print(j);
-                  if(j<i){ 
+                  if(j<i){
                       System.out.print(" + ");
                   }
             }
